afb4843c0172ab9766192d6726a56e322a503cf1
Pankaj P <54427553+PankHub@users.noreply.github.com>
2020-05-02 13:59:52 +0530

    Added test file and modified project structure

---
+++ src/test/java/com/automationtools/TestWDVideoGen.java
@@ -0,0 +1,32 @@
+package com.automationtools;
+
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestWDVideoGen {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver81.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriverVideoRecorder videoRecorder = new WebDriverVideoRecorder(driver);
+		videoRecorder.setRecorder(videoRecorder);
+		videoRecorder.getRecorder().startRecording(System.getProperty("user.dir") + "/target/output", "articleSearch");
+		driver.manage().window().maximize();
+		driver.get("https://techcrunch.com/");
+
+		driver.findElement(By.cssSelector(".search-toggle.search-toggle--search")).click();
+		driver.findElement(By.id("search-box-form__input")).sendKeys("starlink");
+		driver.findElement(By.id("search-box-form__input")).sendKeys(Keys.RETURN);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//body[@id='ysch']//li//li[1]//div[2]//h4[1]//a[1]")).click();
+		try {
+			videoRecorder.stopRecording();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+
+}
